54f1778535d83b71168602b234500efa42bcd0fd
Mitch Jones <mitch@mitchelljones.com>
2020-11-29 16:07:03 -0500

    basic drawing

---
+++ app/static/js/canvas.js
@@ -0,0 +1,73 @@
+/* Adapted from Yusuf Sezer's javascript-canvas-painting - https://github.com/yusufsefasezer/javascript-canvas-painting */
+
+"use strict";
+var canvas = null,
+    btnClear = null,
+    btnSave = null,
+    inputColor = null,
+    inputSize = null,
+    ctx = null,
+    isDrawing = false,
+    lineWidth = 3;
+
+function initialize() {
+    canvas = document.querySelector("#canvas");
+    let canvasDims = canvas.parentElement.getBoundingClientRect();
+    canvas.width = canvasDims.width;
+    canvas.height = canvasDims.height;
+
+    ctx = canvas.getContext("2d");
+
+    btnClear = document.querySelector("#clear");
+    btnSave = document.querySelector("#save");
+    inputColor = document.querySelector("#color");
+    inputSize = document.querySelector("#size");
+
+    window.onmouseup = function () {
+        isDrawing = false;
+        ctx.beginPath();
+    }
+
+    canvas.onmousedown = function (e) {
+        isDrawing = true;
+    }
+
+    canvas.onmousemove = function (e) {
+        if (isDrawing) {
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.lineWidth = lineWidth * 2;
+            ctx.lineCap = "round";
+            ctx.lineJoin = "round";
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(e.offsetX, e.offsetY, lineWidth, 0, 2 * Math.PI, true);
+            ctx.fill();
+            ctx.beginPath();
+            ctx.moveTo(e.offsetX, e.offsetY);
+        }
+    }
+
+    btnClear.onclick = function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    btnSave.onclick = function () {
+        canvas.toBlob(function (blob) {
+            var link = document.createElement("a");
+            link.download = "draw.png";
+            link.href = URL.createObjectURL(blob);
+            link.dispatchEvent(new MouseEvent('click'));
+        }, 'image/png', 1);
+    }
+
+    inputColor.onchange = function () {
+        ctx.fillStyle = this.value;
+        ctx.strokeStyle = this.value;
+    }
+
+    inputSize.onchange = function () {
+        lineWidth = this.value;
+    }
+}
+
+window.onload = initialize;
