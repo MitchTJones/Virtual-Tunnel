dbe4b719e8b5a37aeab204071ac7cd73b9bef9d2
Ariel <hirschhornariel@gmail.com>
2020-12-02 22:13:31 -0500

    basic login/logout features working

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,62 @@
-from flask import render_template
-from app import app
+from flask import Flask, render_template, request
+from app import config
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 @app.route('/')
 def index():
-    user = {'username': 'Bob Smitherson'}
-    return render_template('home.html', user=user)
+    return render_template('home.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# @app.route('/')
+# def index():
+#     user = {'username': 'Bob Smitherson'}
+#     return render_template('home.html', user=user)
+
+# @app.route('/test')
+# def test():
+#     return render_template('test.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/gallery/')
+def gallery():
+    return render_template('view.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- vtunnel.py
+++ vtunnel.py
@@ -1,1 +1,113 @@
-from app import app
+from flask import render_template, request, url_for, redirect
+from flask import Flask
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+application = app
+migrate = Migrate(app, db)
+login = LoginManager(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SECRET_KEY'] = 'hci-2020-hard-to-guess'
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+#db of users
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+#db of posts to the virtual tunnel
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filepath = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.filepath) 
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register/')
+def register():
+    return render_template('signup.html')
+
+@app.route('/paint')
+def paint():
+    return render_template('paint.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('view.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
